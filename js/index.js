e271f2f015ee51b575b2b74fd53d8f12b05c0239
Nahid Hasan Sabbir <sabbirnahid5817@gmail.com>
2021-09-08 12:45:51 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,88 @@
+const foundResult = document.getElementById('found_result');
+const errorMessage = document.getElementById('error_message');
+
+const searchBook = () => {
+  const searchFiled = document.getElementById('search_filed');
+  // Display Spinner
+  document.getElementById('spiner').style.display='block';
+  const searchText = searchFiled.value;
+  // clear data
+  searchFiled.value = '';
+  // Clear found result Details
+  foundResult.innerHTML='';
+  // Clear error Message
+  errorMessage.innerHTML='';
+ 
+   //load data
+    const url = `https://openlibrary.org/search.json?q=${searchText}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displaySearchResult(data))
+
+}
+
+
+
+const displaySearchResult = books => {
+  const searchResult = document.getElementById('show_result')
+  // Clear book Details
+  searchResult.textContent = '';
+
+ // error handal
+  if(books.numFound === 0){
+    // Hide Spinner
+    document.getElementById('spiner').style.display='none';
+    errorMessage.innerHTML=`<h3 class="p-4">No result found!</h3>`
+  }
+  
+ else {
+   // Hide Spinner
+  document.getElementById('spiner').style.display='none';
+  // Display Found Result
+  foundResult.innerHTML=`<h3 class="found_result">Total result found : ${books.numFound}</h3>`
+  
+  // Display Search Result
+  books.docs.forEach(book => {
+    const div = document.createElement('div')
+    div.classList.add('col-md-4')
+    div.innerHTML = `  
+              <div class="card h-100 mb-3 book">
+                <img src="${errorimage(book)}" class="fluid w-50 h-75 mx-auto mt-2" alt="...">
+                <div class="card-body">
+                  <a href=""><h4 class="card-title">${book.title}</h4></a> 
+                  <p class="card-text"> <span  class="author_name"><i>Author:</span> ${book.author_name ?book.author_name:"N/a"}</i></p>
+                  <p class="card-text"> <span  class="publisher">Publisher:</span> ${book.publisher ?book.publisher:"N/a"}</p
+                </div>
+                  <div class="card-footer">
+                  <small class="text-muted"><p class="card-text"><span class="publish_year">First published ${book.first_publish_year ?book.first_publish_year:"N/a"}</span></p></small>
+                  </div>
+              </div>
+            
+            
+        
+        `;
+    searchResult.appendChild(div);
+  });
+
+ }
+
+}
+// error image handal
+const errorimage = book => {
+
+  const imageUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+
+  if (imageUrl === `https://covers.openlibrary.org/b/id/undefined-M.jpg`) {
+
+      const placeHolderImage = "https://openlibrary.org/images/icons/avatar_book-sm.png";
+
+      return placeHolderImage;
+  }
+  else {
+      const mainCoverImage = imageUrl;
+
+      return mainCoverImage;
+
+  }
+
+}
